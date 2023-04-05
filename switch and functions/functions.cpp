991d0a38c6b135ddf2a4db2dfe821654016d4016
yash <yt44151@gmail.com>
2023-04-05 16:44:02 +0530

    functions questions

--- switch and functions/functions.cpp
+++ switch and functions/functions.cpp
@@ -46,19 +46,43 @@ void listprint(int n){
 }
 // sum of n natural numbers
 void sumnatural(int n){
-    cout<<"sum of natural numbers is: "<<;
+    cout<<"sum of natural numbers is: ";
     int sum=0;
     for(int i=1;i<=n;i++){
         sum+=i;
     }
-    cout<<sum;
+    cout<<sum<<endl;
 }
 // sum of first n odd numbers and even numbers
-// calculate nCr = n!/(r!*(n-r)!)
 // factorial
-int fact(int )
+int fact(int n){
+    int ans=1;
+    for(int i=1;i<=n;i++){
+        ans= ans*i;
+    }
+    return ans;
+}
+// calculate nCr = n!/(r!*(n-r)!)
+int ncr(int n, int r){
+    int num= fact(n);
+    int denom= fact(r)*fact(n-r);
+
+    return num/denom;
+}
 
 
 int main(){
+    int n; cin>>n;
+    evenodd(n);
+    isprime(n);
+    sumnatural(n);
+    listprint(n);
+    cout<<"factorial of "<<n<<" is: "<<fact(n)<<endl;
+    cout<<"enter value of r to calculate nCr: ";
+    int r;cin>>r;
+    cout<<"nCr is: "<<ncr(n,r)<<endl;
+    cout<<"enter the value of a and b to calculate a to the power of b: ";
+    int a,b; cin>>a>>b;
+    cout<<"a to the power of b is :"<<pow(a,b)<<endl;
     return 0;
 }
