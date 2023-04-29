1c3953c038af4f196a8b833630bdf4ec2423f160
yash <yt44151@gmail.com>
2023-04-29 19:10:01 +0530

    binary to deccimal code

---
+++ decimalbinary/bianrytodecimal.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a;
+    cin>>n;
+    int ans = 0;
+    int i=0;
+    while(n!=0){
+        a= n%10;
+        ans=(a * pow(2,i))+ans;
+        n=n/10;
+        i++;
+        
+    }
+    cout<<ans;
+ return 0;
+}
--- decimalbinary/decimaltobin.cpp
+++ decimalbinary/decimaltobin.cpp
@@ -8,10 +8,9 @@ int main(){
     int i=0;
     while(n!=0){
         int bit= n&1;
-        ans = (bit * pow(10,i))+ans;
+        ans = (ans * 10)+bit;
         n= n>>1;
         i++;
     }
-
     cout<<ans<<endl;
 }
