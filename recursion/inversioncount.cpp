bee5f2e6186bbe7dba303e7d3a3245c6bdb2105e
yash <yt44151@gmail.com>
2023-05-07 15:57:38 +0530

    inversion count problem using merge sort

--- recursion/inversioncount.cpp
+++ recursion/inversioncount.cpp
@@ -1,61 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int merge(int *arr,int s,int e){
-    int mid= s+(e-s)/2;
-    int n1=mid-s+1;
-    int n2= e-mid;
-    int inv_cnt=0;
-
-    int first[n1];
-    int second[n2];
-
-    // copy first array
-    int k=s;
-    for(int i=0;i<n1;i++){
-        first[i]=arr[k++];
-    }
-    // copy second array
-    k= mid+1;
-    for(int i=0;i<n2;i++){
-        second[i]=arr[k++];
-    }
 
 
-    // this is the same logic for merge two sorted arrays
-    int i=0,j=0;
-    k=s;
-    while(i<n1 && j<n2){
-        if(first[i]<second[j]){
-            arr[k++]=first[i++];
+int merge(int arr[],int temp[],int left,int mid, int right){
+    int i, j, k;
+    int inv_count = 0;
+ 
+    i = left;
+    j = mid;
+    k = left;
+    while ((i <= mid - 1) && (j <= right)) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
         }
-        else{
-            arr[k++]=second[j++];
-            inv_cnt++;
+        else {
+            temp[k++] = arr[j++];
+ 
+            // this is tricky -- see above
+            // explanation/diagram for merge()
+            inv_count = inv_count + (mid - i);
         }
     }
-    while(i<n1){
-        arr[k++]=first[i++];
-    }
-    while(j<n2){
-        arr[k++]=second[j++];
-    }
-    return inv_cnt;
+ 
+    // Copy the remaining elements of left subarray
+    // (if there are any) to temp
+    while (i <= mid - 1)
+        temp[k++] = arr[i++];
+ 
+    // Copy the remaining elements of right subarray
+    // (if there are any) to temp
+    while (j <= right)
+        temp[k++] = arr[j++];
+ 
+    // Copy back the merged elements to original array
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+ 
+    return inv_count;
 }
 
-int mergeSort(int *arr, int s, int e){
+int mergeSort(int arr[], int s, int e){
     int inv_count=0;
-    if(s>=e){
-        return inv_count;
-    }
-    int mid = s+ (e-s)/2;
+    int temp[e+1];
+    if(e> s){
+    int mid = (s+e) /2;
+
     // left part 
     inv_count+= mergeSort(arr, s, mid);
     // right part
     inv_count+= mergeSort(arr, mid+1, e);
 
-    inv_count+= merge(arr,s,e);
-
+    inv_count+= merge(arr,temp,s,mid+1,e);
+    }
+    return inv_count;
     // return inv_count;
 }
 
@@ -70,6 +68,10 @@ int main()
     }
     int inversioncount = mergeSort(arr,0,n-1);
     cout<<"inversion count in the given array is: "<<inversioncount<<endl;
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    // cout<<endl;
 
  return 0;
 }
