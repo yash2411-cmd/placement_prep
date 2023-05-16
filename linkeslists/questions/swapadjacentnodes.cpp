063058c075d16d2d3baf9c80fbde56068ce8c66b
yash <yt44151@gmail.com>
2023-05-16 21:17:10 +0530

    swap adjacent nodes of linked list

---
+++ linkeslists/questions/swapadjacentnodes.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+
+
+//   Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* temp = head->next;
+        head->next= swapPairs(head->next->next);
+        temp->next= head;
+        return temp;
+
+    }
+};
+
+
+int main()
+{
+ return 0;
+}
