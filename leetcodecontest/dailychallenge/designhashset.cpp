012f796b5a44d09e1e79c06b53a703480d6cf9d1
yash <yt44151@gmail.com>
2023-05-30 09:46:55 +0530

    daily challenge leetcode

---
+++ leetcodecontest/dailychallenge/designhashset.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyHashSet {
+public:
+    vector<bool>hash;
+    MyHashSet() {
+        hash.resize(1000001,false);
+    }
+    
+    void add(int key) {
+        hash[key]=true;
+    }
+    
+    void remove(int key) {
+        hash[key]= false;
+    }
+    
+    bool contains(int key) {
+        return hash[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
