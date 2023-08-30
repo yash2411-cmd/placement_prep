479be95f762fad5e079c3a49486296c21c50692d
yash <yt44151@gmail.com>
2023-08-30 19:23:38 +0530

    queue question

---
+++ queues/sum-of-max-and-min-element-in-K-size-window.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int solve(int arr[],int n,int k){
+    deque<int>maxi(k);
+    deque<int>mini(k);
+
+
+    // addition for first k size window
+    int ans=0;
+    for(int i=0;i<k;i++){
+        while(!maxi.empty() && arr[maxi.back()]<=arr[i]){
+            maxi.pop_back();
+        }
+        while(!mini.empty() && arr[mini.back()]>=arr[i]){
+            mini.pop_back();
+        }
+        maxi.push_back(i);
+        mini.push_back(i);
+    }
+
+    // loop me saari windows ko check kro 
+    for(int i=k;i<n;i++){
+        
+        ans += arr[maxi.front()] + arr[mini.front()];
+        // next window
+            // removal
+        while(!maxi.empty() && i-maxi.front() >= k){
+            maxi.pop_front();
+        }
+        while(!mini.empty() && i-mini.front() >= k){
+            mini.pop_front();
+        }
+
+        // addition ka logic
+        while(!maxi.empty() && arr[maxi.back()]<=arr[i]){
+            maxi.pop_back();
+        }
+        while(!mini.empty() && arr[mini.back()]>=arr[i]){
+            mini.pop_back();
+        }
+        maxi.push_back(i);
+        mini.push_back(i);
+
+        // ans += arr[maxi.front()] + arr[mini.front()];
+
+    }
+    ans += arr[maxi.front()] + arr[mini.front()];
+    return ans;
+}
+
+
+int main()
+{
+    int arr[7]={2,5,-1,7,-3,-1,2};
+    int k =4;
+    cout<<solve(arr,7,k)<<endl;
+
+ return 0;
+}
