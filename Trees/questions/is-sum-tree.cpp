71875a34594b849b2e234d65740ad20b8619cd3f
yash <yt44151@gmail.com>
2023-09-01 02:10:33 +0530

    is sum tree or not

---
+++ Trees/questions/is-sum-tree.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; 
+
+// Should return true if tree is Sum Tree, else false
+class Solution
+{
+    public:
+pair<bool,int> isBal(TreeNode* root) {
+        if(root==NULL){
+            pair<bool,int>p = make_pair(true,0);
+            return p;
+        }
+        pair<int,int>left= isBal(root->left);
+        pair<int,int>right= isBal(root->right);
+
+        bool leftans= left.first;
+        bool rightans= right.first;
+        bool diff= abs(left.second - right.second)<=1;
+        
+        pair<bool,int>ans;
+        ans.second = max(left.second,right.second)+1;
+        if(leftans && rightans && diff){
+            ans.first = true;
+        }
+        else{
+            ans.first = false;
+        }
+        return ans;
+    }
+};
+
+
+int main()
+{
+ return 0;
+}
