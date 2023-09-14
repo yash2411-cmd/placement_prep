27ae9f75e82b2389f72273d1aeb918d86ea99f6f
yash <yt44151@gmail.com>
2023-09-14 23:25:54 +0530

    bst quetions

---
+++ BST/questions/two-sum-in-BST.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int> &in){
+    //LNR
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left,in);
+    in.push_back(root->val);
+    inorder(root->right,in);
+}
+
+    bool findTarget(TreeNode* root, int k) {
+        vector<int>in;
+        inorder(root, in);
+        int i=0;
+        int j= in.size()-1;
+
+        while(i<j){
+            int sum = in[i]+in[j];
+            if(sum==k){
+                return true;
+            }
+            else if(sum>k){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+};
+
+
+int main()
+{
+ return 0;
+}
