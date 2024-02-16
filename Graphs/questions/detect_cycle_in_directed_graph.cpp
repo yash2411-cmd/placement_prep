f59f6a2487d104aca421ef4b1e120b21aea1c414
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-02-16 19:13:44 +0530

    cycle detection in graph using BFS

--- Graphs/questions/detect_cycle_in_directed_graph.cpp
+++ Graphs/questions/detect_cycle_in_directed_graph.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+
+// this is using DFS algorithm
 bool isCycle(int node, vector<bool> &visited, vector<bool> &dfsvisited, unordered_map<int, vector<int>> &adj)
 {
     visited[node] = true;
@@ -46,4 +48,66 @@ int detectCycleInDirectedGraph(int n, vector<pair<int, int>> &edges)
         }
     }
     return false;
+}
+
+// this is using BFS algorithm
+int detectCycleInDirectedGraph(int n, vector<pair<int, int>> &edges)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // creating the adjancy list
+    unordered_map<int, vector<int>> adj;
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = edges[i].first - 1;
+        int v = edges[i].second - 1;
+
+        adj[u].push_back(v);
+    }
+    // finding the indegree
+    vector<int> indegree(n);
+    for (auto i : adj)
+    {
+        for (auto j : i.second)
+        {
+            indegree[j]++;
+        }
+    }
+
+    // pushing the nodes which has indegree 0
+    queue<int> q;
+    for (int i = 0; i < n; i++)
+    {
+        if (indegree[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+
+    // bfs for topological sort
+    int count = 0;
+    while (!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+
+        count++;
+
+        for (auto i : adj[front])
+        {
+            indegree[i]--;
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+    }
+    if (count == n)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
