aab5707d18cfcf7f0adc29791f8ce11266f1bff5
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-02-17 01:08:20 +0530

    disjoint sets and kruskal's algorithm implementation to find minimum spanning tree

---
+++ Graphs/questions/Disjoint_set_kruskal's_algo.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+void makeSet(vector<int> &parent, vector<int> &rank, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+int findParent(vector<int> &parent, int node)
+{
+    if (parent[node] == node)
+    {
+        return node;
+    }
+    // applying path compression
+    return parent[node] = findParent(parent, parent[node]);
+}
+bool cmp(vector<int> &a, vector<int> &b)
+{
+    return a[2] < b[2];
+}
+
+void unionSet(int u, int v, vector<int> &parent, vector<int> &rank)
+{
+    u = findParent(parent, u);
+    v = findParent(parent, v);
+
+    if (rank[u] < rank[v])
+    {
+        // change the parent of less rank one
+        parent[u] = v;
+    }
+    // change the parent for less one
+    else if (rank[v] < rank[u])
+    {
+        parent[v] = u;
+    }
+    // if ranks are same change the parent and update the rank of the parent
+    else
+    {
+        parent[v] = u;
+        rank[u]++;
+    }
+}
+
+int minimumSpanningTree(vector<vector<int>> &edges, int n)
+{
+    sort(edges.begin(), edges.end(), cmp);
+    int miniWeight = 0;
+
+    vector<int> parent(n);
+    vector<int> rank(n);
+    makeSet(parent, rank, n);
+
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = findParent(parent, edges[i][0]);
+        int v = findParent(parent, edges[i][1]);
+        int wt = edges[i][2];
+        if (u != v)
+        {
+            miniWeight += wt;
+            unionSet(u, v, parent, rank);
+        }
+    }
+
+    return miniWeight;
+}
+
+int main()
+{
+    return 0;
+}
