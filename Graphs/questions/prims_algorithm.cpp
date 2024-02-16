291d0acc64b5d5abc3f5d3c99be7e9737cef0ef7
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-02-17 00:14:52 +0530

    prims algorithm : optimized using priority queue

--- Graphs/questions/prims_algorithm.cpp
+++ Graphs/questions/prims_algorithm.cpp
@@ -2,10 +2,11 @@
 #include <unordered_map>
 #include <list>
 #include <limits>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-// prims algorithm used for finding the minimum spanning tree
+// prims algorithm used for finding the minimum spanning tree , TC = O(n^2)
 
 vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
 {
@@ -50,6 +51,57 @@ vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pa
                 // update parent and key
                 parent[v] = u;
                 key[v] = w;
+            }
+        }
+    }
+
+    vector<pair<pair<int, int>, int>> ans;
+    for (int i = 2; i <= n; i++)
+    {
+        ans.push_back({{parent[i], i}, key[i]});
+    }
+
+    return ans;
+}
+
+// optimized version uses priority queue for fast reterival of minimum vertex and insertion of that vertex
+vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
+{
+    unordered_map<int, list<pair<int, int>>> adj;
+    for (int i = 0; i < g.size(); i++)
+    {
+        int u = g[i].first.first;
+        int v = g[i].first.second;
+        int w = g[i].second;
+        adj[u].push_back(make_pair(v, w));
+        adj[v].push_back(make_pair(u, w));
+    }
+
+    vector<int> key(n + 1, numeric_limits<int>::max());
+    vector<int> parent(n + 1, -1);
+    vector<bool> inMST(n + 1, false);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    // Start with vertex 1
+    key[1] = 0;
+    pq.push(make_pair(0, 1));
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+        inMST[u] = true;
+
+        for (auto it : adj[u])
+        {
+            int v = it.first;
+            int w = it.second;
+            if (!inMST[v] && w < key[v])
+            {
+                key[v] = w;
+                parent[v] = u;
+                pq.push(make_pair(key[v], v));
             }
         }
     }
