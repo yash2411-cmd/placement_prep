96eff98113226917bc0f41a79efbc93fe388433c
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-02-23 18:25:25 +0530

    cheapest flight with k stops

--- Graphs/questions/cheapest_flight_with_k_stops.cpp
+++ Graphs/questions/cheapest_flight_with_k_stops.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Q: There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+
+// You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
 // using bellman ford algorithm for shortest path
 class ford
 {
