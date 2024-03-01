65c70afd2a3458ade2f6ec4198463f4d83acaccd
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-03-01 19:17:54 +0530

    longets palindromic subsequence

---
+++ DP/longest_palindromic_subsequence.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int opt(string &a, string &b)
+    {
+        int n = a.length();
+        int m = b.length();
+        vector<int> curr(m + 1, 0);
+        vector<int> next(m + 1, 0);
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                int ans = 0;
+                if (a[i] == b[j])
+                {
+                    ans = 1 + next[j + 1];
+                }
+                else
+                {
+                    ans = max(next[j], curr[j + 1]);
+                }
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+    int longestPalindromeSubseq(string s)
+    {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        return opt(s, rev);
+    }
+};
+
+int main()
+{
+    return 0;
+}
