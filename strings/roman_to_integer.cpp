9d789544fc17fa40e6e4b1ca1a15d1051d3f0358
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-03-03 18:21:32 +0530

    Roman to integer

---
+++ strings/roman_to_integer.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int romanToInt(string s)
+    {
+        unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}};
+
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (i < s.size() - 1 && roman[s[i]] < roman[s[i + 1]])
+            {
+                ans -= roman[s[i]];
+            }
+            else
+            {
+                ans += roman[s[i]];
+            }
+        }
+        return ans;
+    }
+};
+int main()
+{
+    return 0;
+}
