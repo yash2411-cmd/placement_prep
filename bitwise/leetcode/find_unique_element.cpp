8e90c024f8b4f89cbb777326fe86fbd4428bd87e
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-03-05 18:17:18 +0530

    find single elementr

---
+++ bitwise/leetcode/find_unique_element.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
