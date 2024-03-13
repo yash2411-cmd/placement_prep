343be9406a890d5153282c062b882e1620e58fd1
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-03-13 23:18:11 +0530

    ninja training

---
+++ DP/ninja_training.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// memoization with time complexity of O(n*4) and space complexity is O(n*4) + O(n) recursion stack space
+int solve(int index, vector<vector<int>> &points, int last, vector<vector<int>> &dp)
+{
+    if (index == 0)
+    {
+        int maxi = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if (i != last)
+            {
+                maxi = max(maxi, points[0][i]);
+            }
+        }
+        return maxi;
+    }
+    if (dp[index][last] != -1)
+    {
+        return dp[index][last];
+    }
+    int maxi = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (i != last)
+        {
+            int point = points[index][i] + solve(index - 1, points, i, dp);
+            maxi = max(point, maxi);
+        }
+    }
+    return dp[index][last] = maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n + 1, vector<int>(4, -1));
+    return solve(n - 1, points, 3, dp);
+}
+
+// tabulation optimized for space complexity
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n, vector<int>(4, 0));
+    dp[0][0] = max(points[0][1], points[0][2]);
+
+    dp[0][1] = max(points[0][0], points[0][2]);
+
+    dp[0][2] = max(points[0][0], points[0][1]);
+
+    dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+    for (int day = 1; day < n; day++)
+    {
+
+        for (int last = 0; last < 4; last++)
+        {
+
+            dp[day][last] = 0;
+
+            for (int task = 0; task < 3; task++)
+            {
+
+                if (task != last)
+                {
+
+                    int point = points[day][task] + dp[day - 1][task];
+
+                    dp[day][last] = max(dp[day][last], point);
+                }
+            }
+        }
+    }
+
+    return dp[n - 1][3];
+    // return solve(n-1, points, 3, dp);
+}
+
+// space optimization
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<int> prev(n, 0);
+    prev[0] = max(points[0][1], points[0][2]);
+
+    prev[1] = max(points[0][0], points[0][2]);
+
+    prev[2] = max(points[0][0], points[0][1]);
+
+    prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+    for (int day = 1; day < n; day++)
+    {
+        vector<int> temp(4);
+        for (int last = 0; last < 4; last++)
+        {
+            temp[last] = 0;
+
+            for (int task = 0; task < 3; task++)
+            {
+
+                if (task != last)
+                {
+
+                    int point = points[day][task] + prev[task];
+
+                    temp[last] = max(temp[last], point);
+                }
+            }
+        }
+        prev = temp;
+    }
+
+    return prev[3];
+}
+
+int main()
+{
+    return 0;
+}
