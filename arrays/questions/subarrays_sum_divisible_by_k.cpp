5eb74fcbc46583aface8d5957303ede67e304b4f
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-03-14 23:15:27 +0530

    number of subarrays sum divisible by k

---
+++ arrays/questions/subarrays_sum_divisible_by_k.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int subarraysDivByK(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        //
+        mp[0] = 1;
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum = (sum + nums[i]) % k;
+            if (sum < 0)
+                sum += k;
+            count += mp[sum];
+            mp[sum]++;
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    return 0;
+}
