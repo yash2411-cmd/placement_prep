145a420a1e721242682bf04b7bda8fc0a9dd2719
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2024-04-17 17:12:30 +0530

    Add one row to tree:-> bfs approach added

--- Binary-Trees/questions/add_one_row_to_tree.cpp
+++ Binary-Trees/questions/add_one_row_to_tree.cpp
@@ -53,6 +53,46 @@ public:
     }
 };
 
+// bfs solution
+TreeNode *addOneRow(TreeNode *root, int val, int depth)
+{
+    if (depth == 1)
+    {
+        TreeNode *newRoot = new TreeNode(val);
+        newRoot->left = root;
+        return newRoot;
+    }
+    queue<TreeNode *> q;
+    q.push(root);
+    int level = 1;
+    while (level < depth - 1 && !q.empty())
+    {
+        int si = q.size();
+        for (int i = 0; i < si; i++)
+        {
+            auto node = q.front();
+            q.pop();
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        level++;
+    }
+    while (!q.empty())
+    {
+        auto node = q.front();
+        q.pop();
+        TreeNode *newleft = new TreeNode(val);
+        newleft->left = node->left;
+        node->left = newleft;
+
+        TreeNode *newright = new TreeNode(val);
+        newright->right = node->right;
+        node->right = newright;
+    }
+    return root;
+}
 int main()
 {
     return 0;
