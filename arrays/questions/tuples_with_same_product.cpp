bb9665a865055e3ffdc9cf44d8e88a6f83281f24
Yash Tiwar <yashtiwari@shooliniuniversity.com>
2025-02-06 20:09:47 +0530

    tuples with same product- leetcode daily

---
+++ arrays/questions/tuples_with_same_product.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int tupleSameProduct(vector<int> &nums)
+    {
+        unordered_map<int, int> mp;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    mp[(nums[i] * nums[j])]++;
+                }
+            }
+        }
+
+        for (auto i : mp)
+        {
+            if (i.second > 1)
+            {
+                ans += (i.second * (i.second - 1) / 2) * 8;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v = {2, 3, 4, 6};
+    cout << s.tupleSameProduct(v) << endl;
+    return 0;
+}
